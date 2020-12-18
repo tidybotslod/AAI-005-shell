4d0240c5ed932674ab447245b0fc7dd4189e893c
tidybots <tidybotslod@outlook.com>
2020-12-18 10:10:27 -0800

    Bundle up the necessary dependencies into a bat file

---
+++ QnA/dependencies.bat
@@ -0,0 +1,5 @@
+dotnet add package System.CommandLine --version 2.0.0-beta1.20371.2
+dotnet add package Microsoft.Azure.CognitiveServices.Knowledge.QnAMaker --version 2.0.1
+dotnet add package Microsoft.Extensions.Configuration
+dotnet add package Microsoft.Extensions.Configuration.CommandLine
+dotnet add package Microsoft.Extensions.Configuration.Json
