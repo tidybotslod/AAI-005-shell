ee8ae229decd5d61e623a21e8f9e6a677307bd8d
tidybots <tidybotslod@outlook.com>
2020-12-18 09:39:42 -0800

    Remove project files - students will recreate them

--- QnA/Program.cs
+++ QnA/Program.cs
@@ -10,45 +10,6 @@ namespace QnA
 {
     public class Program
     {
-        QnAService QnA;
-
-        public Program()
-        {
-            QnA = new QnAService();
-        }
-
-        public QnAService Service { get { return QnA; } }
-
-        public async Task<bool> CreateQnA(string file)
-        {
-            return false;
-        }
-
-        public async Task<bool> AddToQnA(string file, bool published)
-        {
-            return false;
-        }
-
-        public async Task<bool> UpdateQnA(string file, bool published)
-        {
-            return false;
-        }
-
-        public async Task Train(string file, bool published)
-        {
-        }
-        public async Task Publish()
-        {
-        }
-
-        public async Task Ask(string question, bool published, int number)
-        {
-        }
-        public async Task DeleteKnowledgeBase()
-        {
-            await QnA.DeleteKnowledgeBase();
-        }
-
         static void Main(string[] args)
         {
 
@@ -58,6 +19,9 @@ namespace QnA
             {
 
             };
+
+            rootCommand.Description = "";
+            
             rootCommand.Handler = CommandHandler.Create(() =>
             {
 
